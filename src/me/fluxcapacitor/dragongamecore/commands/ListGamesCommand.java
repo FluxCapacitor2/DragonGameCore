1c9475e84908607774ab3909ad3c90819ce86549
FluxCapacitor <tntawkward@gmail.com>
2019-07-04 19:03:25 -0400

    Added support for the all games GUI.

--- src/me/fluxcapacitor/dragongamecore/commands/ListGamesCommand.java
+++ src/me/fluxcapacitor/dragongamecore/commands/ListGamesCommand.java
@@ -5,13 +5,19 @@ import me.fluxcapacitor.dragongamecore.Main;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 public class ListGamesCommand implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        sender.sendMessage(Main.colorize("&bHere are all the games you can play:"));
-        for (DragonGame game : Main.games) {
-            sender.sendMessage(Main.colorize("    &b" + game.getName()));
+        if (sender instanceof Player) {
+            Player player = (Player) sender;
+            Main.instance.guiManager.gamesInv.open(Main.games.get(0), player);
+        } else {
+            sender.sendMessage(Main.colorize("&bAll loaded games on this server:"));
+            for (DragonGame game : Main.games) {
+                sender.sendMessage(Main.colorize("    &b" + game.getName()));
+            }
         }
         return true;
     }
