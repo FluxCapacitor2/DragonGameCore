5c1d52648b4c4a0fcecf1c3c805c7a77dab64478
FluxCapacitor <tntawkward@gmail.com>
2019-07-04 18:53:22 -0400

    Updated class to correctly use new DebugAPI version.

--- src/me/fluxcapacitor/dragongamecore/Debug.java
+++ src/me/fluxcapacitor/dragongamecore/Debug.java
@@ -8,18 +8,18 @@ import me.skater77i.bluedragon.debug.DebugAPI;
 public class Debug {
 
     public static void verbose(String string) {
-        DebugAPI.log("all", string, 0);
+        DebugAPI.log(string, 0);
     }
 
     public static void info(String string) {
-        DebugAPI.log("all", string, 1);
+        DebugAPI.log(string, 1);
     }
 
     public static void warn(String string) {
-        DebugAPI.log("all", string, 2);
+        DebugAPI.log(string, 2);
     }
 
     public static void error(String string) {
-        DebugAPI.log("all", string, 3);
+        DebugAPI.log(string, 3);
     }
 }
